37569aa51fb8e8bc8cc25c3efbb5f07b439d5bf0
AlaAydi <contact.aydiala@gmail.com>
2026-05-09 17:21:46 +0100

    correct sonarqub

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,5 +1,8 @@
-@props(['value'])
+@props(['value', 'for' => null])
 
-<label {{ $attributes->merge(['class' => 'block font-semibold text-sm text-slate-300 mb-2']) }}>
+<label
+    @if($for) for="{{ $for }}" @endif
+    {{ $attributes->merge(['class' => 'block font-semibold text-sm text-slate-300 mb-2']) }}
+>
     {{ $value ?? $slot }}
 </label>
